49d2634adbe34d958c80b0cbfab54edcb622a1b3
Pelumi Adebayo <padebayo@microsoft.com>
2023-07-25 23:35:51 +0100

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,66 @@
+﻿import $ from "jquery";
+import React, { Component, createRef } from "react";
+
+window.jQuery = $;
+window.$ = $;
+
+require("jquery-ui-sortable");
+require("formBuilder");
+
+
+
+const fields = [{
+    label: 'Star Rating',
+    attrs: {
+        type: 'starRating'
+    },
+    icon: '🌟'
+}];
+const templates = {
+    starRating: function (fieldData) {
+        return {
+            field: '<span id="' + fieldData.name + '">',
+            onRender: function () {
+                $(document.getElementById(fieldData.name)).rateYo({ rating: 3.6 });
+            }
+        };
+    }
+};
+
+
+const formData = [
+    {
+        type: "header",
+        subtype: "h1",
+        label: "formBuilder in React"
+    },
+    {
+        type: "paragraph",
+        label: "This is a demonstration of formBuilder running in a React project."
+    }
+];
+
+const getData=()=>{
+    let  fb = createRef();
+  alert(this.fb.current);
+  }
+export class Counter extends Component {
+    fb = createRef();
+    componentDidMount() {
+        //$(this.fb.current).formBuilder({ formData });
+
+        $(this.fb.current).formBuilder({ formData, fields, templates });
+
+      
+    }
+
+    
+    render() {
+        return (
+        <div>
+            <button onClick={getData}>Done</button>
+            <div className="fb-editor" ref={this.fb} />
+        </div>
+        );
+    }
+}
